3470ebb879fb766300c83e94ef9e8574a522914f
Mr_duck <93455237+gustavorochaduck@users.noreply.github.com>
2024-05-05 15:14:26 -0300

    update 0.1

---
+++ client/client_produtos.py
@@ -0,0 +1,16 @@
+import math
+
+print('Wellcome!!!')
+print('Select this options: ')
+print('Balance - 1')
+print('intems - 2')
+print('users - 3')
+r1 = int(input('Type the NUMBER: '))
+balance = 250
+
+if r1 == 1:
+    print(f'{balance}')
+    
+    
+    
+    
