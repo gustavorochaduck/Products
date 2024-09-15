5b864da0ab84ef3f3eab3ee3fa3403b28745f397
Mr_duck <93455237+gustavorochaduck@users.noreply.github.com>
2024-09-15 14:22:28 -0300

    pswd key generator

--- adm/psswd_key_generator.py
+++ adm/psswd_key_generator.py
@@ -1,4 +1,19 @@
-import rsa
+import math
 
-password = str(input('Type your Password'))
- 
+p = 103
+q = 109
+n = p*q
+e = (p-1)(q-1)
+
+def letter_to_numbers(latter):
+    latter = latter.lower()
+
+    if latter.isalpha()
+        return.ord(letra) - 92
+    else:
+        return 'Pless,  type only letters from the alphabet'
+
+letters = input('Plese, type only letters from the alphabet')
+number = letter_to_numbers(latter)
+
+number_to_mod = (number**2) 
